bb63a7d9a512e960c4cbbb06c5337b0946dcc9a8
David Malcolm <dmalcolm@redhat.com>
2011-09-28 14:35:14 -0400

    bump to 3.1.0
    * Wed Sep 28 2011 David Malcolm <dmalcolm@redhat.com> - 3.1.0-1
    - 3.1.0; converting from arch-specific to noarch (sitearch -> sitelib);
    removing rdfpipe and various other extensions

--- filter-provides.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# See http://fedoraproject.org/wiki/PackagingDrafts/FilteringAutomaticDependencies
-if [ -x /usr/lib/rpm/redhat/find-provides ] ; then
-FINDPROV=/usr/lib/rpm/redhat/find-provides
-else
-FINDPROV=/usr/lib/rpm/find-provides
-fi
-
-$FINDPROV $* | sed -e '/SPARQLParserc.so/d'
